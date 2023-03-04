f44c204eebdb8a0a3836b4232eaee4b7f5969caa
Andrew Nasef <androamazing@gmail.com>
2023-03-05 01:41:35 +0200

    change api key

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,2 +1,2 @@
 export const WEATHER_URL = "https://api.weatherapi.com/v1"
-export const WEATHER_APIKEY = "41ffbd305d38429abe772200230403"
+export const WEATHER_APIKEY = "e2f01d22f4554cebb74233943230403"
